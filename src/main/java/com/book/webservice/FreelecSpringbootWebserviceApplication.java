fd18c94623df70d812805e5fdfaba42d1e29bb46
HOYEONCHOI <chy1995@ajou.ac.kr>
2022-09-10 00:14:33 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/book/webservice/FreelecSpringbootWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.book.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringbootWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelecSpringbootWebserviceApplication.class, args);
+	}
+
+}
