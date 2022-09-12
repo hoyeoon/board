a5441e7513da15c20c07b6d937406631ea0b6940
HOYEONCHOI <chy1995@ajou.ac.kr>
2022-09-13 01:59:59 +0900

    feat: 게시글 등록 API

---
+++ src/main/java/com/book/webservice/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.book.webservice.service.posts;
+
+import com.book.webservice.domain.posts.PostsRepository;
+import com.book.webservice.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
