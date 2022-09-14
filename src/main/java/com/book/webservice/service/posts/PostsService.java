5aabddda7d5386c4ffd05c153e6dc243c48c21b3
HOYEONCHOI <chy1995@ajou.ac.kr>
2022-09-15 00:18:48 +0900

    feat: 게시글 수정, 조회 API

--- src/main/java/com/book/webservice/service/posts/PostsService.java
+++ src/main/java/com/book/webservice/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.book.webservice.service.posts;
 
+import com.book.webservice.domain.posts.Posts;
 import com.book.webservice.domain.posts.PostsRepository;
+import com.book.webservice.web.dto.PostsResponseDto;
 import com.book.webservice.web.dto.PostsSaveRequestDto;
+import com.book.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +18,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
---
+++ src/main/java/com/book/webservice/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.book.webservice.web.dto;
+
+import com.book.webservice.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
